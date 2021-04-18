eff3402a76b392ff83809e47a0167e878a073814
TranexTrane <denoncinedouard@hotmail.be>
2021-04-18 19:12:25 +0200

    Push portfolio 19h 18-04-21

--- src/js/myJs.js
+++ src/js/myJs.js
@@ -1,2 +1,10 @@
-var hello = "hello";
-console.log(`${hello} world`);
+var myBurger    = document.querySelector('.burger-menu');
+var btnBurger = document.querySelector('.btn-burger');
+    btnBurger.addEventListener('click', function() {   
+        myBurger.classList.add("active");
+});
+
+let close = document.querySelector('.close')
+      close.addEventListener('click', function() {
+        myBurger.classList.remove("active");
+  });
